0a2ea7838e62d3ede7a2492949d23cd3a023c842
Administrator <sylvert.foyang@advance-it-group.biz>
2018-09-23 14:17:47 +0100

    version correct finalisé

---
+++ src/main/java/com/example/demo/controller/ProduitContoller.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Produit;
+
+@RestController
+@RequestMapping("/api/produit")
+public class ProduitContoller extends CrudController<Produit, Long>{
+
+}
---
+++ src/main/java/com/example/demo/security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.example.demo.security;
+
+public class SecurityConstants {
+
+	public static final String SECRET="orion@Assurance2018";
+	public static final long EXPIRATION_TIME = 1000_000_000; //10 jours
+	public static final String TOKEN_PREFIX ="Bearer ";
+	public static final String HEADER_STRING = "Authorization";
+}
